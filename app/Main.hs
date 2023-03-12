a1af03ad2669a7aa040cc64757ccd9cd62ed0974
Philg112 <pgrujovski@protonmail.com>
2023-03-12 13:56:58 +1100

    brain explosion

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Main where
 
 import Data.Functor
@@ -176,8 +178,49 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+     do result <- eval pred
+        case result of
+            Bool False -> eval alt
+            _ -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x:xs)] = return x
+car [DottedList (x:xs) _] = return x
+car [badArg] = throwError $ TypeMismatch "pair" badArg
+car badArgList = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x:xs)] = return $ List xs
+cdr [DottedList [_] x] = return x
+cdr [DottedList (_:xs) x] = return $ DottedList xs x
+cdr [badArg] = throwError $ TypeMismatch "pari" badArg
+cdr badARgList = throwError $ NumArgs 1 badARgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List[]] = return $ List [x1]
+cons [x, List xs] = return $ List $ x:xs
+cons [x, DottedList xs xlast] = return $ DottedList (x:xs) xlast
+cons [x1, x2] = return $ DottedList [x1] x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
+    where
+        eqvPair :: (LispVal, LispVal) -> Bool
+        eqvPair (x1, x2) =
+            case eqv [x1,x2] of
+                Left err -> False
+                Right (Bool val) -> val
+eqv [_,_] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 3 badArgList
 
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives = [("+", numericBinop (+)),
@@ -199,7 +242,13 @@ primitives = [("+", numericBinop (+)),
                 ("string<?", strBoolBinop (<)),
                 ("string>?", strBoolBinop (>)),
                 ("string<=?", strBoolBinop (<=)),
-                ("string>=?", strBoolBinop (>=))]
+                ("string>=?", strBoolBinop (>=)),
+                ("car", car),
+                ("cdr", cdr),
+                ("cons", cons),
+                ("eq?", eqv),
+                ("eqv?", eqv),
+                ("equal?", equal)]
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
@@ -250,6 +299,22 @@ unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
                                 else return $ fst $ head parsed
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker f) = do
+    unpacked1 <- f arg1
+    unpacked2 <- f arg2
+    return $ unpacked1 == unpacked2
+    `catchError` const (return False)
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+    primEquals <- or <$> mapM (\u -> unpackEquals arg1 arg1 u) [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+    eqvEquals <- eqv [arg1, arg2]
+    return $ Bool $ (primEquals || let (Bool x) = eqvEquals in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
 
 data LispError = NumArgs Integer [LispVal]
                | TypeMismatch String LispVal
